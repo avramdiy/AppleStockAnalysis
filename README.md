7d4d751965c7a571d8511e3c94f80cbe690af1d7
Avram Diyaljee <avram.diyaljee@gmail.com>
2025-04-04 07:55:05 -1000

    1st Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# TRG Week 18
+
+## AAPL Stock Analysis
+
+- Link to Dataset : https://www.kaggle.com/datasets/varpit94/apple-stock-data-updated-till-22jun2021
+
+### 1st Commit
+
+- Loaded the initial Flask / Python code to process the .csv into an HTML dataframe for cleaning.
+
+### 2nd Commit
+
+### 3rd Commit
+
+### 4th Commit
+
+### 5th Commit
---
+++ app/data.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template_string
+import pandas as pd
+
+app = Flask(__name__)
+
+DATA_PATH = r'C:\Users\Ev\Desktop\TRG Week 18\AAPL.csv'
+
+@app.route('/')
+def index():
+    # Read CSV into DataFrame
+    df = pd.read_csv(DATA_PATH)
+
+    # Convert DataFrame to HTML
+    table_html = df.to_html(classes='table table-bordered table-hover', index=False)
+
+    # HTML page
+    html_page = '''
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <title>AAPL CSV Viewer</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    </head>
+    <body class="p-4">
+        <h1 class="mb-4">AAPL.csv Data</h1>
+        <div class="table-responsive">
+            {{ table|safe }}
+        </div>
+    </body>
+    </html>
+    '''
+
+    return render_template_string(html_page, table=table_html)
+
+if __name__ == '__main__':
+    app.run(debug=True)
